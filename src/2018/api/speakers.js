33e5d08dd7e87b0b65dd29778c15a909fb0d7fee
Andrew Rota <anrota@wayfair.com>
2018-07-30 13:45:25 -0400

    Add speakers

--- src/2018/api/speakers.js
+++ src/2018/api/speakers.js
@@ -1,6 +1,14 @@
 import wheeler from '../images/ken_wheeler.jpg';
 import vieira from '../images/sara_vieira.jpg';
 import djirdeh from '../images/houssein_djirdeh.jpg';
+import rival from '../images/florian_rival.jpg';
+import kumar from '../images/tejas_kumar.jpg';
+import sapegin from '../images/artem_sapegin.jpg';
+import bretous from '../images/samantha_bretous.jpg';
+import hamil from '../images/matt_hamil.jpg';
+import ward from '../images/francois_ward.jpg';
+import luker from '../images/jen_luker.jpg';
+import riemer from '../images/vincent_riemer.jpg';
 
 export const speakers = [
   {
@@ -23,5 +31,61 @@ export const speakers = [
     company: '',
     twitter: 'https://twitter.com/hdjirdeh',
     github: 'https://github.com/housseindjirdeh'
+  },
+  {
+    name: 'Vincent Riemer',
+    src: riemer,
+    company: 'Symantec',
+    twitter: 'https://twitter.com/vincentriemer',
+    github: 'https://github.com/vincentriemer'
+  },
+  {
+    name: 'Samantha Bretous',
+    src: bretous,
+    company: 'MLS',
+    twitter: 'https://twitter.com/samanthabretous',
+    github: 'https://github.com/samanthabretous'
+  },
+  {
+    name: 'Matt Hamil',
+    src: hamil,
+    company: '20|20 Research',
+    twitter: 'https://twitter.com/_matthamil',
+    github: 'https://github.com/matthamil'
+  },
+  {
+    name: 'Jen Luker',
+    src: luker,
+    company: 'Formidable',
+    twitter: 'https://twitter.com/knitcodemonkey',
+    github: 'https://github.com/knitcodemonkey'
+  },
+  {
+    name: 'Artem Sapegin',
+    src: sapegin,
+    company: 'Wayfair',
+    twitter: 'https://twitter.com/iamsapegin',
+    github: 'https://github.com/sapegin'
+  },
+  {
+    name: 'Tejas Kumar',
+    src: kumar,
+    company: '',
+    twitter: 'https://twitter.com/tejaskumar_',
+    github: 'https://github.com/tejasq'
+  },
+  {
+    name: 'Florian Rival',
+    src: rival,
+    company: 'Facebook',
+    twitter: 'https://twitter.com/Florianrival',
+    github: 'https://github.com/4ian'
+  },
+  {
+    name: 'Francois Ward',
+    src: ward,
+    company: 'HubSpot',
+    twitter: 'https://twitter.com/Florianrival',
+    github: 'https://github.com/Phoenixmatrix'
   }
 ];
