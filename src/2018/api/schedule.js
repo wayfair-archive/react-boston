4e44e252f89e5bc44b698ce63ad9538848f5b6ea
Andrew Rota <anrota@wayfair.com>
2018-08-07 09:26:12 -0400

    Add title

--- src/2018/api/schedule.js
+++ src/2018/api/schedule.js
@@ -169,7 +169,7 @@ This talk will explain all the problems I had and solutions that I used to port 
   {
     time: '3:00 - 3:30',
     day: 2,
-    title: 'TBA',
+    title: 'Deterministic mocking with GraphQL and Apollo: Lunar Launch',
     speaker: 'Hillary Bauer and Mark Faga',
     isSavable: false,
     description: `Tests are an integral part of the development life cycle, but it isn’t always clear what the most efficient way to write them is. Learn some of the Dos & Don'ts when it comes to testing your React app with Apollo components. We’ll explore: When is a unit test appropriate? When should you lean on integration tests? And how the heck do you dynamically mock GraphQL responses with data specific to your tests? ezCater has solved these problems and is happy to announce its newest edition to the open source community: Lunar, a package to help you mock GraphQL data for your integration tests!`
