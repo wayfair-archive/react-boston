720be73e58f2a683a8e1c2faae8dbcc3adcd9805
Eric Hermanson <ehermanson@wayfair.com>
2019-04-05 12:26:09 -0400

    update react-dom to fix possible vulnerability

--- src/index.js
+++ src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { unregister } from './registerServiceWorker';
 
-import injectTapEventPlugin from 'react-tap-event-plugin';
-
-// Needed for onTouchTap
-// http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
-
 ReactDOM.render(<App />, document.getElementById('root'));
 unregister();
