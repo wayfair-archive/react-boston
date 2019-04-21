72d21014bfaa6e8e5888f2b79b309f8f6ed1eb0f
Eric <eric.r.hermanson@gmail.com>
2019-04-21 10:12:41 -0400

    favicon

--- 2019/gatsby-config.js
+++ 2019/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `React Boston 2019`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `New England's annual React conference!`,
     author: `@wayfair`,
   },
   plugins: [
--- 2019/src/components/seo.js
+++ 2019/src/components/seo.js
@@ -9,6 +9,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Helmet from "react-helmet"
 import { useStaticQuery, graphql } from "gatsby"
+import favicon from "../images/favicon.ico"
 
 function SEO({ description, lang, meta, keywords, title }) {
   const { site } = useStaticQuery(
@@ -34,6 +35,9 @@ function SEO({ description, lang, meta, keywords, title }) {
       }}
       title={title}
       titleTemplate={`%s | ${site.siteMetadata.title}`}
+      link={[
+        { rel: "shortcut icon", type: "image/x-icon", href: `${favicon}` },
+      ]}
       meta={[
         {
           name: `description`,
