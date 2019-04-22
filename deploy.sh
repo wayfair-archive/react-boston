55e2375b3f7fca1afae45f98040fe86f468afdad
Daniel Hartman <dhartman1@wayfair.com>
2019-04-22 11:26:07 -0400

    Fix deploy scrip and 2018 PUBLIC_URL

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #/bin/bash
 set -euo pipefail
 
-git subtree push --prefix build origin gh-pages
+git checkout master
+# create a local gh-pages branch containing the splitted output folder
+git subtree split --prefix build -b gh-pages
+# force the push of the gh-pages branch to the remote gh-pages branch at origin
+git push -f origin gh-pages:gh-pages
+# delete the local gh-pages
+git branch -D gh-pages
