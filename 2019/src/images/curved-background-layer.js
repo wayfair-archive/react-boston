d30ad98b413290792175ca7fa1a81bec2ba02cbd
Sam Morrison <sam@sammorrison.net>
2019-04-24 18:21:15 -0400

    Fix Curvy Image (#11)
    * fix curvey image
    * update svg display to cover gaps

--- 2019/src/images/curved-background-layer.js
+++ 2019/src/images/curved-background-layer.js
@@ -1,7 +1,7 @@
 import React from "react"
 
 const CurvedBackgroundLayer = () => (
-  <svg viewBox="0 0 437 437" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg viewBox="0 0 437 437" fill="none" xmlns="http://www.w3.org/2000/svg" display="block">
     <path d="M0 0s1 174.5 219 219 218 218 218 218H0V0z" fill="#fff" />
   </svg>
 )
