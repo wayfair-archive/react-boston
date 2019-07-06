6489688bb25176b78cb11478dc38f9e8b1b56a7d
Kelly Bondanza <kbondanza@wayfair.com>
2019-07-06 14:16:37 -0400

    layout tweeks

--- 2019/src/pages/sponsors.js
+++ 2019/src/pages/sponsors.js
@@ -5,21 +5,30 @@ import Link from "../components/link"
 
 export default () => (
   <Layout>
-    <div>
-      <Text mb={4}>React Boston 2019 is sponsored and hosted by:</Text>
-      <Text mb={4}>Wayfair</Text>
-      <Text mb={4}>
+    <Box maxWidth={["94vw", "90vw", "", "1200px"]} my={11} mx="auto">
+      <Text mb={4} fontSize={3}>
+        React Boston 2019 is sponsored and hosted by:
+      </Text>
+      <Text mb={4} fontSize={3}>
+        Wayfair
+      </Text>
+      <Text mb={4} fontSize={3}>
         We're the creative builders solving complex business problems through
         the newest and most innovative software in e-commerce.
       </Text>
-      <Box mb={4}>
+      <Text mb={4} fontSize={3}>
         <Link href="https://tech.wayfair.com/" openInNewTab>
           tech.wayfair.com
         </Link>
-      </Box>
-      <Link href="https://www.wayfair.com/careers/jobs/?teamIds=1" openInNewTab>
-        wayfair.com/careers
-      </Link>
-    </div>
+      </Text>
+      <Text fontSize={3}>
+        <Link
+          href="https://www.wayfair.com/careers/jobs/?teamIds=1"
+          openInNewTab
+        >
+          wayfair.com/careers
+        </Link>
+      </Text>
+    </Box>
   </Layout>
 )
