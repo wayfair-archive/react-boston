bf0580207429434df61ab1d29f3aa6e0ec723898
Eric Hermanson <ehermanson@wayfair.com>
2019-07-08 16:02:43 -0400

    small button style updates

--- 2019/src/components/speakers/speaker-modal.js
+++ 2019/src/components/speakers/speaker-modal.js
@@ -3,12 +3,14 @@ import { Box, Text } from "../layout-components"
 import Modal from "../modal"
 import Link from "../link"
 import { Twitter, Github } from "../../images/icons/social"
+import { css } from "@emotion/core"
 import styled from "@emotion/styled"
 
 const StyledImage = styled.img`
   display: block;
   height: 400px;
   width: 400px;
+  border-bottom-right-radius: 50%;
 `
 
 const DescriptionList = Box.withComponent("dl")
@@ -30,31 +32,38 @@ export default function SpeakerModal({
     <Modal onRequestClose={onRequestClose} isOpen={isOpen}>
       <Box display="flex">
         <StyledImage src={src} alt={name} />
-        <DescriptionList mt="4">
-          <Name fontWeight="bold">{name}</Name>
-          <CompanyName color="mediumGrey">{company}</CompanyName>
-          <div>
-            <dd>
-              <Link href={twitter}>
-                <Twitter
-                  fill="#f15959"
-                  width="28px"
-                  height="28px"
-                  title="Twitter"
-                />
-              </Link>
-              <Link href={github}>
-                <Github
-                  fill="#f15959"
-                  width="28px"
-                  height="28px"
-                  title="Twitter"
-                />
-              </Link>
-            </dd>
-          </div>
-        </DescriptionList>
-        description
+        <Box
+          p={7}
+          css={css`
+            flex: 1;
+          `}
+        >
+          <DescriptionList>
+            <Name fontWeight="bold">{name}</Name>
+            <CompanyName color="mediumGrey">{company}</CompanyName>
+            <div>
+              <dd>
+                <Link href={twitter}>
+                  <Twitter
+                    fill="#f15959"
+                    width="28px"
+                    height="28px"
+                    title="Twitter"
+                  />
+                </Link>
+                <Link href={github}>
+                  <Github
+                    fill="#f15959"
+                    width="28px"
+                    height="28px"
+                    title="Twitter"
+                  />
+                </Link>
+              </dd>
+            </div>
+          </DescriptionList>
+          description
+        </Box>
       </Box>
     </Modal>
   )
