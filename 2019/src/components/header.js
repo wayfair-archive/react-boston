fe378734e11a541c937f7ad3b0e87d12140f6287
Artem Sapegin <asapegin@wayfair.com>
2019-07-10 10:33:56 +0200

    Second attempt at the header

--- 2019/src/components/header.js
+++ 2019/src/components/header.js
@@ -1,150 +1,193 @@
 import React, { useState } from "react"
 import { Link } from "gatsby"
+import styled from "@emotion/styled"
 import { css } from "@emotion/core"
+import { Box, Title } from "./layout-components"
 import Button from "./button"
-
 import LogoText from "../images/logo-text"
 import FullLogo from "../images/icons/full-logo"
-import Logo from "../images/logo"
+import LogoImage from "../images/logo"
+import Kebab from "../images/icons/kebab"
 
-const NavLink = props => (
-  <Link
-    css={css`
-      display: block;
-      text-align: center;
-      padding: 10px;
-      text-decoration: none;
-      @media screen and (min-width: 1100px) {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-      }
-    `}
-    activeStyle={{ textDecoration: "underline" }}
+// TODO: Show shadow on stick
+// TODO: Responsive
+// TODO: Use the right logo
+// TODO: Show logo only when stick
+// TODO: Use Button component from ./button
+
+const LINKS = [
+  {
+    title: "About",
+    href: "/about",
+  },
+  {
+    title: "Speakers",
+    href: "/speakers",
+  },
+  {
+    title: "Schedule",
+    href: "/schedule",
+  },
+  {
+    title: "Venue & Travel",
+    href: "/venue",
+  },
+  {
+    title: "Diversity",
+    href: "/diversity",
+  },
+  {
+    title: "Sponsors",
+    href: "/sponsors",
+  },
+]
+
+const Container = props => (
+  <Box as="header" position="sticky" top={0} bg="white" {...props} />
+)
+
+const Logo = ({ children, ...props }) => (
+  <Box
+    as="a"
+    display="block"
+    width={195}
+    px={8}
+    py={4}
+    aria-label={children}
+    {...props}
+  >
+    <LogoText />
+  </Box>
+)
+
+const Nav = props => (
+  <Box
+    as="nav"
+    py={10}
+    px={11}
+    color="white"
+    bg="primaryDark"
+    borderRadius="0 0 0 65px"
     {...props}
   />
 )
 
-const Header = () => {
+const NavItem = styled(Box)`
+  list-style: none;
+`
+
+const NavLinkBase = styled(Link)`
+  color: ${p => p.theme.colors.white};
+  text-decoration: none;
+
+  &:hover,
+  &:focus,
+  &:active {
+    border-bottom: 2px solid;
+  }
+
+  &:focus {
+    outline-offset: 2px;
+    outline: 1px dotted;
+  }
+`
+
+const NavLink = props => (
+  <NavLinkBase
+    {...props}
+    activeStyle={{
+      borderBottom: "2px solid",
+      fontWeight: "bold",
+    }}
+  />
+)
+
+const BuyButton = styled.a`
+  display: block;
+  padding: ${p => p.theme.space[4]}px ${p => p.theme.space[9]}px;
+  background-color: ${p => p.theme.colors.secondary};
+  color: ${p => p.theme.colors.black};
+  font-size: ${p => p.theme.fontSizes[3]}px;
+  font-style: italic;
+  font-weight: bold;
+  text-decoration: none;
+  border-radius: 2em;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: ${p => p.theme.colors.secondaryDark};
+  }
+
+  &:focus {
+    outline: 0;
+  }
+`
+
+const MenuButtonBase = styled(Box)`
+  display: inline-block;
+  background: none;
+  border: 0;
+  padding: 0;
+  margin: 0;
+  cursor: pointer;
+  color: ${p => p.theme.colors.white};
+
+  &:active,
+  &:hover,
+  &:focus {
+    outline-offset: 2px;
+    outline: 1px dotted;
+  }
+`
+
+const MenuButton = props => (
+  <MenuButtonBase as="button" aria-label="Menu" {...props}>
+    <Kebab />
+  </MenuButtonBase>
+)
+
+const Header = ({ siteTitle, ...props }) => {
   const [isOpen, setIsOpen] = useState(false)
   const toggleMenu = () => setIsOpen(isOpen => !isOpen)
   return (
-    <header
-      css={css`
-        display: grid;
-        grid-template-columns: 1fr 2fr;
-        @media screen and (min-width: 1100px) {
-          grid-template-columns: 20% 1fr;
-        }
-      `}
-    >
-      <Link
-        to="/"
-        css={css`
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          padding: 20px 10px;
-        `}
-      >
-        <Logo
-          css={css`
-            width: 200px;
-            max-width: 100%;
-          `}
-        />
-        {/* <LogoText
-          css={css`
-            max-width: 175px;
-          `}
-        /> */}
-        {/* <FullLogo /> */}
-      </Link>
-
-      <div
-        css={theme => css`
-          background-color: ${theme.colors.primaryDark};
-          padding: 10px 10px;
-          color: white;
-          border-bottom-left-radius: 65px;
-          @media screen and (min-width: 1100px) {
-            padding: 0;
-            background-color: transparent;
-          }
-        `}
-      >
-        <button
-          css={css`
-            display: block;
-            position: ${isOpen ? "fixed" : "static"};
-            height: 60px;
-            width: 60px;
-            top: 10px;
-            z-index: 2;
-            background: white;
-            right: 10px;
-            margin-left: auto;
-            @media screen and (min-width: 1100px) {
-              display: none;
-            }
-          `}
-          onClick={toggleMenu}
-        >
-          menu
-        </button>
-        <nav
-          css={theme => css`
-            background-color: ${theme.colors.primaryDark};
-            padding: 40px;
-            display: ${isOpen ? "flex" : "none"};
-            position: fixed;
-            flex-direction: column;
-            padding-top: 100px;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            z-index: 1;
-            height: 100%;
-            border-bottom-left-radius: 50vw;
-            box-shadow: 0 20px 20px rgba(0, 0, 0, 0.6);
-            @media screen and (min-width: 1100px) {
-              padding: 0 40px 0 60px;
-              flex-direction: row;
-              position: static;
-              display: flex;
-              flex-wrap: nowrap;
-              align-items: center;
-              justify-content: space-between;
-              border-bottom-left-radius: 65px;
-              box-shadow: none;
-            }
-            a {
-              color: white;
-            }
-          `}
-        >
-          <NavLink to="/speakers">Speakers</NavLink>
-          <NavLink to="/schedule">Schedule</NavLink>
-          <NavLink to="/venue">Venue & Travel</NavLink>
-          <NavLink to="/diversity">Diversity</NavLink>
-          <NavLink to="/sponsors">Sponsors</NavLink>
-          <NavLink to="/conduct">Conduct</NavLink>
-          <NavLink to="/about">About</NavLink>
-          <Button
-            css={css`
-              order: -1;
-              @media screen and (min-width: 1100px) {
-                order: 1;
-              }
-            `}
-            href="https://www.eventbrite.com/e/react-boston-2019-tickets-61701810777"
+    <Container {...props}>
+      <Box display="flex">
+        <Logo href="/">{siteTitle}</Logo>
+        <Nav display={[null, null, null, "flex"]} flexGrow="1" fontSize={2}>
+          <Box
+            display={["flex", null, null, "none"]}
+            alignItems="flex-end"
+            flexDirection="column"
+            mb={4}
           >
-            Buy Tickets
-          </Button>
-        </nav>
-      </div>
-    </header>
+            <MenuButton onClick={toggleMenu} />
+          </Box>
+          <Box
+            as="ul"
+            display={[isOpen ? "flex" : "none", null, null, "flex"]}
+            flexDirection={["column", null, null, "row"]}
+            alignItems={["center", null, null, "baseline"]}
+          >
+            {LINKS.map(({ title, href }) => (
+              <NavItem
+                as="li"
+                key={href}
+                p={[3, null, null, 0]}
+                mr={[0, null, null, 6]}
+              >
+                <NavLink to={href}>{title}</NavLink>
+              </NavItem>
+            ))}
+            <Box ml={[0, null, null, 8]} mt={[6, null, null, 0]}>
+              <BuyButton href="https://www.eventbrite.com/e/react-boston-2019-tickets-61701810777">
+                Buy Tickets
+              </BuyButton>
+            </Box>
+          </Box>
+        </Nav>
+      </Box>
+    </Container>
   )
 }
 
---
+++ 2019/src/images/icons/kebab.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Kebab = () => (
+  <svg
+    viewBox="0 0 24 24"
+    width="40"
+    height="40"
+    xmlns="http://www.w3.org/2000/svg"
+    display="block"
+    aria-hidden="true"
+  >
+    <path
+      d="M24 6h-24v-4h24v4zm0 4h-24v4h24v-4zm0 8h-24v4h24v-4z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+export default Kebab
