30495358cefee3617f5b537e74c3a3422064ed44
Kelly Bondanza <kbondanza@wayfair.com>
2019-07-11 10:47:24 -0400

    tweaks to about page

--- 2019/src/pages/about.js
+++ 2019/src/pages/about.js
@@ -16,7 +16,6 @@ const Image = styled(Img)`
   border-radius: 0 65px 0 65px;
   width: 100%;
   box-shadow: 10px 20px 20px rgba(0, 0, 0, 0.15);
-  margin-bottom: 40px;
 `
 
 export default () => {
@@ -39,30 +38,39 @@ export default () => {
   return (
     <Layout>
       <Container title="About React Boston">
-        <Image
-          fluid={data.bostonImage.childImageSharp.fluid}
-          alt="Boston, MA"
-        />
-        <Text
-          mb={10}
-          css={css`
-            a {
-              ${linkStyles}
-            }
-          `}
-          dangerouslySetInnerHTML={{
-            __html: data.site.siteMetadata.about,
-          }}
-        />
-        <SectionTitle mb={2}>Past Years</SectionTitle>
-        <Text mb={2}>Have a look at our previous conferences:</Text>
-        <Box as="ul" pl={8}>
-          <li>
-            <Link href="https://www.reactboston.com/2017/">2017</Link>
-          </li>
-          <li>
-            <Link href="https://www.reactboston.com/2018/">2018</Link>
-          </li>
+        <Box
+          display="grid"
+          gridGap={9}
+          gridTemplateColumns={["1fr", "1fr 1fr"]}
+          alignItems="center"
+        >
+          <Image
+            fluid={data.bostonImage.childImageSharp.fluid}
+            alt="Boston, MA"
+          />
+          <Box>
+            <Text
+              mb={10}
+              css={css`
+                a {
+                  ${linkStyles}
+                }
+              `}
+              dangerouslySetInnerHTML={{
+                __html: data.site.siteMetadata.about,
+              }}
+            />
+            <SectionTitle mb={2}>Past Years</SectionTitle>
+            <Text mb={2}>Have a look at our previous conferences:</Text>
+            <Box as="ul" pl={8}>
+              <li>
+                <Link href="https://www.reactboston.com/2017/">2017</Link>
+              </li>
+              <li>
+                <Link href="https://www.reactboston.com/2018/">2018</Link>
+              </li>
+            </Box>
+          </Box>
         </Box>
       </Container>
     </Layout>
