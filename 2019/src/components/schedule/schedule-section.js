911b404983015cbce96992e7cc603bf5e169a94f
Kelly Bondanza <kbondanza@wayfair.com>
2019-09-19 20:16:02 -0400

    Added a toggle to show full schedule on landing

--- 2019/src/components/schedule/schedule-section.js
+++ 2019/src/components/schedule/schedule-section.js
@@ -1,12 +1,22 @@
-import React from "react"
+import React, { Fragment, useState } from "react"
 import { useStaticQuery, graphql } from "gatsby"
 import { Box, SectionTitle, Text } from "../layout-components"
-import Button from "../button"
+import { Tabs, Tab, TabPanel } from "../tabs"
 import styled from "@emotion/styled"
 
 const SECOND_DAY = "September 22 2019 00:00:00 EST"
 
 const CURRENT_DATE = new Date()
+
+const Wrapper = styled.div`
+  display: grid;
+  grid-gap: 10px;
+  grid-template-columns: 1fr 1fr;
+  margin-bottom: 28px;
+  @media screen and (min-width: 72em) {
+    margin-bottom: 64px;
+  }
+`
 
 const ScheduleWrap = styled.section`
   color: ${({ theme }) => theme.colors.white};
@@ -45,7 +55,6 @@ const DescriptionDefinition = styled.dd`
   }
   @media screen and (min-width: 80em) {
     max-width: 100%;
-    white-space: nowrap;
   }
 `
 
@@ -92,10 +101,15 @@ export default function ScheduleSection() {
     }
   })
 
-  const scheduleToDisplay = () => {
-    const dayToFilter = CURRENT_DATE >= SECOND_DAY ? 2 : 1
-    return schedule.filter(program => program.day === dayToFilter)
-  }
+  const firstDaySchedule = schedule.filter(program =>
+    program.day === 1 ? program : null
+  )
+
+  const secondDaySchedule = schedule.filter(program =>
+    program.day === 2 ? program : null
+  )
+
+  const [isSelectedTab, setSelectedTab] = useState(false)
 
   return (
     <ScheduleWrap>
@@ -107,36 +121,78 @@ export default function ScheduleSection() {
       >
         Schedule
       </SectionTitle>
-      <Box
-        as="dl"
-        display={[null, null, null, "grid"]}
-        gridTemplateColumns="30% 70%"
-        gridGap={10}
-        px={8}
-      >
-        {scheduleToDisplay().map(({ time, title, abstract, talk }) => {
-          return (
-            <React.Fragment key={time}>
-              <DescriptionTerm>
-                <TextDecoration>{time}</TextDecoration>
-              </DescriptionTerm>
-              <DescriptionDefinition>
-                <Text lineHeight="50px" fontSize={[4, null, null, 5]}>
-                  {title}
-                </Text>
-                {talk && (
-                  <p>
-                    {talk.node.name} - {talk.node.company}
-                  </p>
-                )}
-              </DescriptionDefinition>
-            </React.Fragment>
-          )
-        })}
-      </Box>
-      <Box p="12">
-        <Button to="/schedule">Full Schedule</Button>
-      </Box>
+      <Tabs initialValue={1}>
+        <Wrapper role="tablist">
+          <Tab activeTab={1} onTabClick={() => setSelectedTab(isSelectedTab)}>
+            Saturday, September 21
+          </Tab>
+          <Tab activeTab={2} onTabClick={() => setSelectedTab(isSelectedTab)}>
+            Sunday, September 22
+          </Tab>
+        </Wrapper>
+        <TabPanel activeTab={1} is="div">
+          <Box
+            display={[null, null, null, "grid"]}
+            gridTemplateColumns="30% 70%"
+            gridGap={10}
+            px={8}
+            as="dl"
+            mb={12}
+            maxWidth="750px"
+          >
+            {firstDaySchedule.map(({ time, title, abstract, talk }) => {
+              return (
+                <Fragment key={time}>
+                  <DescriptionTerm>
+                    <TextDecoration>{time}</TextDecoration>
+                  </DescriptionTerm>
+                  <DescriptionDefinition>
+                    <Text lineHeight="50px" fontSize={[4, null, null, 5]}>
+                      {title}
+                    </Text>
+                    {talk && (
+                      <p>
+                        {talk.node.name} - {talk.node.company}
+                      </p>
+                    )}
+                  </DescriptionDefinition>
+                </Fragment>
+              )
+            })}
+          </Box>
+        </TabPanel>
+        <TabPanel activeTab={2} is="div">
+          <Box
+            display={[null, null, null, "grid"]}
+            gridTemplateColumns="30% 70%"
+            gridGap={10}
+            px={8}
+            as="dl"
+            mb={12}
+            maxWidth="750px"
+          >
+            {secondDaySchedule.map(({ time, title, abstract, talk }) => {
+              return (
+                <Fragment key={time}>
+                  <DescriptionTerm>
+                    <TextDecoration>{time}</TextDecoration>
+                  </DescriptionTerm>
+                  <DescriptionDefinition>
+                    <Text lineHeight="50px" fontSize={[4, null, null, 5]}>
+                      {title}
+                    </Text>
+                    {talk && (
+                      <p>
+                        {talk.node.name} - {talk.node.company}
+                      </p>
+                    )}
+                  </DescriptionDefinition>
+                </Fragment>
+              )
+            })}
+          </Box>
+        </TabPanel>
+      </Tabs>
     </ScheduleWrap>
   )
 }
